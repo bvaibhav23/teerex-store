671c8532e79d8894c5136f6ce2d525bdc34f3a8c
VAIBHAV BHASKAR <vaibhavbhaskar23@gmail.com>
2023-03-03 23:10:53 +0530

    cart total update

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,15 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import QuantityManager from "./QuantityManager";
 
 const Cart = ({ cartItem, items, IncreaseCount, DecreaseCount, count, removeItem }) => {
+    let [total, setTotal] = useState(Number(0));
+    useEffect(() => {
+        let sum = 0;
 
+        cartItem.map((val) => sum += (Number(val.price) * count[val.id - 1]))
+        setTotal(sum)
+    },
+        [cartItem, count])
     return <>
         {cartItem.length === 0 && <h1 className="container text-center mt-5 shadow p-2 ">Empty Cart</h1>}
         {cartItem.length > 0 &&
@@ -26,13 +33,15 @@ const Cart = ({ cartItem, items, IncreaseCount, DecreaseCount, count, removeItem
                                 <td><img src={val.imageURL} style={{ width: "5rem", height: "7rem" }} alt="" /></td>
                                 <td className="pt-3" ><p className="d-flex flex-column fw-bold">{val.name}</p></td>
                                 <td className="pt-3" ><div style={{ backgroundColor: `${val.color}`, width: "2rem", height: "2rem" }}></div></td>
-                                <td className="pt-3">{val.price}</td>
+                                <td className="pt-3"><p>{`${val.price} * ${count[val.id - 1]}`}</p></td>
                                 <td><QuantityManager val={val} items={items}
                                     count={count}
                                     IncreaseCount={IncreaseCount}
                                     DecreaseCount={DecreaseCount} /></td>
                                 <td id="total" className="pt-3"><button className="btn btn-danger" onClick={() => removeItem(val)}>X</button></td>
-                            </tr>)}
+                            </tr>)
+                        }
+                        <tr><th className="text-center" colSpan={4}><h3>Total</h3></th><th colSpan={3}><h3>{total}</h3></th></tr>
                     </tbody>
 
                 </table>
