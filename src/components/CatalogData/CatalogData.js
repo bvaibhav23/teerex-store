1a6919a8d3a1f7936feb2072642950abd1703998
bvaibhav23 <108619671+bvaibhav23@users.noreply.github.com>
2023-03-15 19:58:50 +0530

    Update CatalogData.js

--- src/components/CatalogData/CatalogData.js
+++ src/components/CatalogData/CatalogData.js
@@ -132,7 +132,7 @@ const CatalogData = ({ items, IncreaseCount, DecreaseCount, count }) => {
           />
         </div>
 
-        <div className="col-10">
+        <div className={show ? "col-10" : ""}>
           <div className="d-flex flex-wrap m-3 justify-content-center">
             {items
               .filter((val) => {
